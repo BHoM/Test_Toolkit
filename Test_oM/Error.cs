c46bf41c94af6f2436803ff941b6c933a95017c1
Andrew Wakeman <andrew.wakeman@burohappold.com>
2019-10-16 12:08:36 +0100

    Add Error object to oM and simple engine methods

---
+++ Test_oM/Error.cs
@@ -0,0 +1,16 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Test
+{
+    public class Error : BHoMObject
+    {
+        public ErrorLevel Level { get; set; }
+        public string Message { get; set; }
+        public Span Location { get; set; }
+    }
+}
