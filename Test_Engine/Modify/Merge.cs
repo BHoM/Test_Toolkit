ea1d212abe648989c00475f26250b6b09cc514ef
Andrew Wakeman <andrew.wakeman@burohappold.com>
2019-10-18 12:02:39 +0100

    Refactor Merge to be compliant

--- Test_Engine/Modify/Merge.cs
+++ Test_Engine/Modify/Merge.cs
@@ -34,14 +34,11 @@ namespace BH.Engine.Test
     {
         public static ComplianceResult Merge(this ComplianceResult a, ComplianceResult b)
         {
-            List<Error> errors = a.Errors;
-            if (errors == null) errors = new List<Error>();
-            if (b.Errors != null) errors = errors.Concat(b.Errors).ToList();
-            return new ComplianceResult
-            {
-                Errors = errors,
-                Status = (ResultStatus)Math.Max((int)a.Status, (int)b.Status)
-            };
+            a = a.GetShallowClone() as ComplianceResult;
+            if (a.Errors == null) a.Errors = new List<Error>();
+            a.Errors.AddRange(b.Errors);
+            if (b.Status > a.Status) a.Status = b.Status;
+            return a;
         }
     }
 }
