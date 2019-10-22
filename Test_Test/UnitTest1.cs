08a27990c0df42b1d96e51759bd5b6ff0afc4bc6
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-10-22 11:39:46 +0100

    Update Test_Test project

--- Test_Test/UnitTest1.cs
+++ Test_Test/UnitTest1.cs
@@ -6,9 +6,19 @@ namespace Test_Test
     [TestClass]
     public class UnitTest1
     {
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+
         [TestMethod]
         public void TestMethod1()
         {
+            TestContext.WriteLine("true");
             Assert.IsTrue(true);
         }
     }
