7fe8a7f3d4d330d4010ac2e00814c14caca138b6
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-10-22 14:06:03 +0100

    Add changes pre nuget

--- Test_Test/UnitTest1.cs
+++ Test_Test/UnitTest1.cs
@@ -1,21 +1,58 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+using BH.oM.Test;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Test_Test
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
         {
-            Assert.IsTrue(true);
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void TestMethod1()
         {
-            Assert.IsTrue(false);
+            string projectName = TestContext.Properties["projectName"].ToString();
+
+            if (projectName == "null") return;
+
+            if (projectName.EndsWith("_Toolkit"))
+                projectName = projectName.Split('_')[0];
+
+            string cwd = Directory.GetCurrentDirectory();
+            Console.WriteLine(cwd);
+
+            string projectOM = Path.Combine("..", "..", projectName + "_Toolkit", projectName + "_oM");
+            string projectEngine = Path.Combine("..", "..", projectName + "_Toolkit", projectName + "_Engine");
+            string projectAdapter = Path.Combine("..", "..", projectName + "_Toolkit", projectName + "_Adapter");
+            string projectUI = Path.Combine("..", "..", projectName + "_Toolkit" + projectName + "_UI");
+
+            List<string> oMFiles = Directory.EnumerateFiles(projectOM, "*.cs", SearchOption.AllDirectories).ToList();
+
+            ComplianceResult result = null;
+            foreach(string s in oMFiles)
+            {
+                StreamReader sr = new StreamReader(s);
+                
+                sr.Close();
+            }
+
+
+            Assert.IsTrue(true);
         }
     }
 }
