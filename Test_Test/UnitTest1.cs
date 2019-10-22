789b64f877d4079107f42539fd83af7ec9a3894c
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-10-22 11:44:15 +0100

    Test a failing case

--- Test_Test/UnitTest1.cs
+++ Test_Test/UnitTest1.cs
@@ -6,20 +6,11 @@ namespace Test_Test
     [TestClass]
     public class UnitTest1
     {
-        private TestContext testContextInstance;
-
-        public TestContext TestContext
-        {
-            get { return testContextInstance; }
-            set { testContextInstance = value; }
-        }
-
-
         [TestMethod]
         public void TestMethod1()
         {
-            TestContext.WriteLine("true");
             Assert.IsTrue(true);
+            Assert.IsTrue(false);
         }
     }
 }
