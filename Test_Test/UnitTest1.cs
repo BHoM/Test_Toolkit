7ba6e3a123b01ebb6b3188c19f18d4f58a09829e
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-10-23 10:55:10 +0100

    Debug attempt

--- Test_Test/UnitTest1.cs
+++ Test_Test/UnitTest1.cs
@@ -63,12 +63,13 @@ namespace Test_Test
                 StreamReader sr = new StreamReader(s);
 
                 string file = sr.ReadToEnd();
-                //sr.Close();
+                sr.Close();
                 if (file != null)
                 {
                     //Assert.Fail(file);
                     SyntaxTree st = BH.Engine.Test.Convert.ToSyntaxTree(file, s);
-                    result = result.Merge(st.GetFileRoot().RunChecks());
+                    Assert.Fail(st.GetFileRoot().GetType().ToString());
+                    //result = result.Merge(st.GetFileRoot().RunChecks());
                 }
 
                 sr.Close();
