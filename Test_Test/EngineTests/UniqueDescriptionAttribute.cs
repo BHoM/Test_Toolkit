d593f92bf0e17d829571d725fa9403ce2f401d97
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-11-18 12:42:37 +0000

    #71 add engine unit tests

---
+++ Test_Test/EngineTests/UniqueDescriptionAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Test.Test
+{
+    public partial class Test_Engine
+    {
+        [TestMethod]
+        public void UniqueDescriptionAttribute()
+        {
+            Test.RunTest("UniqueDescriptionAttribute", GetChangedObjectFiles());
+        }
+    }
+}
