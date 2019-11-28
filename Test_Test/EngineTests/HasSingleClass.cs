b02e0f9853f7022ca562a619afa14b18dba0492b
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-11-28 14:54:25 +0000

    Add unit tests for checking single class

---
+++ Test_Test/EngineTests/HasSingleClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Test.Test
+{
+    public partial class Test_Engine
+    {
+        [TestMethod]
+        public void HasSingleClass()
+        {
+            Test.RunTest("HasSingleClass", GetChangedObjectFiles());
+        }
+    }
+}
