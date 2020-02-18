ff8ef1d2acecc7a2deb7b2598f6a17379305f2d2
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2020-02-18 22:12:27 +0000

    Update HasMachingInputDescription to account for new quantities attributes

--- CodeComplianceTest_Engine/Query/Checks/HasMatchingInputDescription.cs
+++ CodeComplianceTest_Engine/Query/Checks/HasMatchingInputDescription.cs
@@ -45,7 +45,7 @@ namespace BH.Engine.Test.CodeCompliance.Checks
             {
                 foreach (var ab in method.InputAttributes())
                 {
-                    if (ab.Name.ToString() == "Input" && ab.ArgumentList.Arguments.Count == 2)
+                    if (ab.Name.ToString() == "Input" && ab.ArgumentList.Arguments.Count >= 2)
                     {
                         string paramname = ab.ArgumentList.Arguments[0].Expression.GetFirstToken().Value.ToString();
                         if (paramname == node.Identifier.ToString())
