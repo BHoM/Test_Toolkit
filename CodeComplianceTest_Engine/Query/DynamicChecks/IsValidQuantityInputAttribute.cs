8635eff0806402d84fd25635c2dd9bee4ee2d214
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-02-28 09:19:08 +0000

    Add quantity attribute compiled compliance check #147

---
+++ CodeComplianceTest_Engine/Query/DynamicChecks/IsValidQuantityInputAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Test;
+using BH.oM.Quantities.Attributes;
+
+namespace BH.Engine.Test.CodeCompliance.DynamicChecks
+{
+    public static partial class Query
+    {
+        public static bool IsValidQuantityInputAttribute(MemberInfo node)
+        {
+            List<InputAttribute> inputAttributes = node.GetCustomAttributes<InputAttribute>().ToList();
+
+            foreach(InputAttribute ia in inputAttributes)
+            {
+                if(ia.Quantity != null)
+                {
+                    if (!typeof(QuantityAttribute).IsAssignableFrom(ia.Quantity.GetType()))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
