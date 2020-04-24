e061b8d3e97c9771eb4e0d595da4445b2d44f45e
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-04-24 12:29:21 +0100

    Force compliance failure

--- CodeComplianceTest_Engine/Query/Checks/HasDescriptionAttribute.cs
+++ CodeComplianceTest_Engine/Query/Checks/HasDescriptionAttribute.cs
@@ -40,6 +40,7 @@ namespace BH.Engine.Test.CodeCompliance.Checks
         [Path(@"([a-zA-Z0-9]+)_Engine\\.*\.cs$")]
         [Path(@"([a-zA-Z0-9]+)_Engine\\Objects\\.*\.cs$", false)]
         [IsPublic()]
+        [Input("notNode", "Blah")]
         public static Span HasDescriptionAttribute(this MethodDeclarationSyntax node)
         {
             return (node.IsDeprecated() || node.HasAttribute("Description")) ? null : node.Identifier.Span.ToSpan();
