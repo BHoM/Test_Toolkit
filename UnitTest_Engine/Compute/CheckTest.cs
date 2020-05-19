958f9c43cbb27721de7b635bac58ffeaef250dea
Isak Naslund <Isak.Naslund@burohappold.com>
2020-05-19 19:18:26 +0200

    Migrating IsEqual methods from Interop test, and return diffing results from checking method

--- UnitTest_Engine/Compute/CheckTest.cs
+++ UnitTest_Engine/Compute/CheckTest.cs
@@ -34,6 +34,8 @@ using Microsoft.CSharp.RuntimeBinder;
 using UT = BH.oM.Test.UnitTests;
 using BH.oM.Reflection.Interface;
 using BH.oM.Reflection;
+using BH.oM.Diffing;
+using BH.oM.Test.Results;
 
 namespace BH.Engine.UnitTest
 {
@@ -45,28 +47,29 @@ namespace BH.Engine.UnitTest
 
         [Description("Executes a unit test and compares the result of running the method with provided test data with the corresponding expected outputs.")]
         [Input("test", "The test to run, containing the method to be executed as well as the data to test on.")]
-        [MultiOutput(0, "runErrors", "Any errors encountered during the execution of the method.")]
-        [MultiOutput(1, "comparisonErrors", "Any errors encountered during the comparison of the expected output data with the result of running the method.")]
-        public static Output<List<string>, List<string>> CheckTest(this UT.UnitTest test)
+        [MultiOutput(0, "results", "Results from the comparison of the run data with the expected output.")]
+        [MultiOutput(1, "errors", "Any errors encountered during the execution of the method or during the comparison of the expected output data with the result of running the method.")]
+        public static Output<List<DiffingResult>, List<string>> CheckTest(this UT.UnitTest test)
         {
 
             List<List<object>> results = new List<List<object>>();
-            List<string> runErrors = new List<string>();
-            List<string> comparisonErrors = new List<string>();
+            List<string> errors = new List<string>();
+            List<DiffingResult> comparisonResults = new List<DiffingResult>();
             MethodBase method = test.Method;
+            DiffConfig diffConfig = new DiffConfig();
             foreach (UT.TestData data in test.Data)
             {
                 var result = Run(method, data);
 
                 if (result.Item2.Count != 0)
-                    runErrors.AddRange(result.Item2);
+                    errors.AddRange(result.Item2);
                 else
                 {
                     try
                     {
                         if (result.Item1.Count != data.Outputs.Count)
                         {
-                            comparisonErrors.Add("Running of the method returned a different number of results compared to the expected output.");
+                            errors.Add("Running of the method returned a different number of results compared to the expected output.");
                             continue;
                         }
 
@@ -75,19 +78,27 @@ namespace BH.Engine.UnitTest
                             object resultObj = result.Item1[i];
                             object refObject = CastToType(data.Outputs[i], resultObj.GetType());
 
-                            if(!Engine.Test.Query.IsEqual(resultObj, refObject))
-                                comparisonErrors.Add("Didn't return the correct output for output " + i);
+                            var diffResult = Engine.Test.Query.IsEqual(resultObj, refObject, diffConfig);
+
+                            DiffingResult diff = new DiffingResult { Name = "Output " + i, IsEqual = diffResult.Item1 };
+
+                            for (int j = 0; j < diffResult.Item2.Count; j++)
+                            {
+                                diff.Differences.Add(new PropertyDifference { Name = diffResult.Item2[j], FirstItemValue = diffResult.Item3[j], SecondItemValue = diffResult.Item4[j] });
+                            }
+
+                            comparisonResults.Add(diff);
                         }
 
                     }
                     catch
                     {
-                        comparisonErrors.Add("Failed to run the comparison");
+                        errors.Add("Failed to run the comparison");
                     }
                 }
 
             }
-            return new Output<List<string>, List<string>> { Item1 = runErrors, Item2 = comparisonErrors };
+            return new Output<List<DiffingResult>, List<string>> { Item1 = comparisonResults, Item2 = errors };
         }
     
 
