86bc8b8be184aeac2e74d21221ed23b5b3f093a2
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-08-26 15:36:40 +0100

    Update dependencies

--- BuildTest.ps1
+++ BuildTest.ps1
@@ -19,7 +19,7 @@ $slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
 
 # **** Building .sln ****
 write-Output ("Building " + $repo + ".sln")
-& $msbuildPath -nologo $slnPath /verbosity:minimal"
+& $msbuildPath -nologo $slnPath /verbosity:minimal
 
 # **** Building BHoM Test_Toolkit ****
 $repo = "Test_Toolkit"
