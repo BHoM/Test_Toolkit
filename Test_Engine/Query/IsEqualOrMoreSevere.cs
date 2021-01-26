94286860ec9ea8b8cc6ad35e09ad6b3229e7d833
Isak Näslund <isak.naslund@burohappold.com>
2021-01-26 08:51:08 +0100

    Apply suggestions from code review
    Co-authored-by: Fraser Greenroyd <fraser.greenroyd@burohappold.com>

--- Test_Engine/Query/IsEqualOrMoreSevere.cs
+++ Test_Engine/Query/IsEqualOrMoreSevere.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -36,7 +36,7 @@ namespace BH.Engine.Test
         /**** Public Methods                            ****/
         /***************************************************/
 
-        [Description("Checks if a TestStatus is equally or more sever to a comparison status.")]
+        [Description("Checks if a TestStatus is equally or more severe to a comparison status.")]
         [Input("status", "The status to check if it is worse or equal to the other.")]
         [Input("statusToCompareTo", "The status to compare with.")]
         [Output("result", "Returns true if the status is worse or equal in severity compared to the comparison status. Returns false if the status is undefined.")]
