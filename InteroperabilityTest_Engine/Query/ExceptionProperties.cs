4ea4357f7020c5a6c96f03d46a4b46907a2fac16
Isak Naslund <Isak.Naslund@burohappold.com>
2021-02-01 12:59:30 +0100

    Adding method for outputting failing properties

---
+++ InteroperabilityTest_Engine/Query/ExceptionProperties.cs
@@ -0,0 +1,107 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Base;
+using BH.oM.Test.Results;
+using BH.oM.UnitTest.Results;
+using BH.oM.Test.Interoperability.Results;
+using BH.oM.Reflection;
+using BH.Engine.Test;
+using BH.oM.Test;
+
+namespace BH.Engine.Test.Interoperability
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("")]
+        [Input("", "")]
+        [MultiOutput(0, "errors", "The properties that show up as errors in the test.")]
+        [MultiOutput(1, "warnings", "The properties that show up as warnings in the test.")]
+        [MultiOutput(2, "passes", "The properties that show up as passes in the test.")]
+        public static Output<List<string>, List<string>, List<string>> ExceptionProperties(TestResult pushPullCompareDiffingResults, bool onlyLastProperty = false)
+        {
+            List<ComparisonDifference> diffrences = GetComparisonsDiffrences(pushPullCompareDiffingResults);
+
+            List<string> errors = new List<string>();
+            List<string> warnings = new List<string>();
+            List<string> passes = new List<string>();
+
+            foreach (var group in diffrences.GroupBy(x => ExceptionPropertiesGroupingKey(x.Property, onlyLastProperty)))
+            {
+                TestStatus status = group.MostSevereStatus();
+
+                switch (status)
+                {
+                    case TestStatus.Error:
+                        errors.Add(group.Key);
+                        break;
+                    case TestStatus.Pass:
+                        passes.Add(group.Key);
+                        break;
+                    case TestStatus.Warning:
+                        warnings.Add(group.Key);
+                        break;
+                }
+            }
+
+            return new Output<List<string>, List<string>, List<string>>() { Item1 = errors, Item2 = warnings, Item3 = passes };
+        }
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        private static List<ComparisonDifference> GetComparisonsDiffrences(TestResult result)
+        {
+            List<ComparisonDifference> comparisons = result.Information.OfType<ComparisonDifference>().ToList();
+
+            foreach (TestResult res in result.Information.OfType<TestResult>())
+            {
+                comparisons.AddRange(GetComparisonsDiffrences(res));
+            }
+            return comparisons;
+        }
+
+        /***************************************************/
+
+        private static string ExceptionPropertiesGroupingKey(string propertyId, bool onlyLastProperty)
+        {
+            if (onlyLastProperty)
+            {
+                return propertyId.Split('.').Last();
+            }
+            else
+                return propertyId;
+        }
+
+        /***************************************************/
+    }
+}
