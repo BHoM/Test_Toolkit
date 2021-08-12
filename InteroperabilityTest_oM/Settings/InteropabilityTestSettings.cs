27a46064e4b9ea7e76f98558668f456e2347d14f
Isak Naslund <Isak.Naslund@burohappold.com>
2021-08-12 13:49:07 +0200

    Adding InteropabilityTestSettings object

---
+++ InteroperabilityTest_oM/Settings/InteropabilityTestSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using BH.oM.Base;
+
+namespace BH.oM.Test.Interoperability.Settings
+{
+    [Description("All settings needed to run a PushPullCompare for a specific adapter.")]
+    public class InteropabilityTestSettings : BHoMObject
+    {
+        [Description("The type of adapter to test.")]
+        public virtual Type AdapterType { get; set; }
+
+        [Description("The arguments required by the constructor of the adapter with the largest set of arguments.")]
+        public virtual List<object> AdapterConstructorArguments { get; set; } = new List<object>();
+
+        [Description("The types of objects to be tested")]
+        public virtual List<Type> TestTypes { get; set; } = new List<Type>();
+
+        [Description("Config to be used for the PushPullCompare method.")]
+        public virtual PushPullCompareConfig PushPullConfig { get; set; }
+    }
+}
