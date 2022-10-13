0a2e7ed6e56f2efc447bf65001deef235d1d2244
Isak Naslund <Isak.Naslund@burohappold.com>
2022-10-13 11:30:53 +0200

    Update FIlterByCheckStatus to return unmatches as well as matches

--- UnitTest_Engine/Query/FilterByCheckStatus.cs
+++ UnitTest_Engine/Query/FilterByCheckStatus.cs
@@ -49,15 +49,20 @@ namespace BH.Engine.UnitTest
         [Input("test", "The tests to filter.")]
         [Input("status", "Status looked for.")]
         [Input("filterData", "If true, returns new UnitTests objects where the data has been filtered to only contain TestData which returns the provided status. If false, returns full provided UnitTest if the overall status is matching the provided status.")]
-        [MultiOutput(0, "tests", "The filtered tests.")]
-        [MultiOutput(1, "results", "Results matching the filtered tests.")]
-        public static Output<List<UT.UnitTest>, List<TestResult>> FilterByCheckStatus(this List<UT.UnitTest> tests, BH.oM.Test.TestStatus status = oM.Test.TestStatus.Error, bool filterData = false)
+        [MultiOutput(0, "matchingTests", "The filtered tests with status matching the provided status.")]
+        [MultiOutput(1, "matchingResults", "Results matching the filtered tests.")]
+        [MultiOutput(2, "unmatchingTests", "Tests with a status other than the provided.")]
+        [MultiOutput(3, "unmatchingResults", "TestResults corresponding to the UnitTests not matching the status.")]
+        public static Output<List<UT.UnitTest>, List<TestResult>, List<UT.UnitTest>, List<TestResult>> FilterByCheckStatus(this List<UT.UnitTest> tests, BH.oM.Test.TestStatus status = oM.Test.TestStatus.Error, bool filterData = false)
         {
             tests = tests.Where(x => x != null).ToList();   //Filter out nulls
 
             //Return collections
-            List<UT.UnitTest> filteredTests = new List<UT.UnitTest>();
-            List<TestResult> filteredResults = new List<TestResult>();
+            List<UT.UnitTest> matchingTests = new List<UT.UnitTest>();
+            List<TestResult> matchingResults = new List<TestResult>();
+
+            List<UT.UnitTest> unmatchingTests = new List<UT.UnitTest>();
+            List<TestResult> unmatchingResults = new List<TestResult>();
 
             if (filterData) //If filterData is true, some work is required
             {
@@ -65,28 +70,47 @@ namespace BH.Engine.UnitTest
                 {
                     TestResult result = test.CheckTest();   //Get result from check test
 
-                    if (result.Information.All(x => x.Status == status))    //If all inner reuslts match the status, the full test and result can be outputed
+                    if (result.Information.All(x => x.Status == status))    //If all inner reuslts match the status, the full test and result can be outputed. Nothing to add to the unmatching
                     {
-                        filteredTests.Add(test);
-                        filteredResults.Add(result);
+                        matchingTests.Add(test);
+                        matchingResults.Add(result);
                     }
                     else    //If not, need to investigate inner
                     {
-                        UT.UnitTest filterTest = new UT.UnitTest { Method = test.Method };  //Halfclone of the UT, keeping only the method
-                        TestResult filterResult = result.ShallowClone();    //Clone the result and wipe inner TestInformation
-                        filterResult.Information = new List<oM.Test.ITestInformation>();
+                        //Set up output results for matches
+                        UT.UnitTest matchTest = new UT.UnitTest { Method = test.Method };  //Halfclone of the UT, keeping only the method
+                        TestResult matchResult = result.ShallowClone();    //Clone the result and wipe inner TestInformation
+                        matchResult.Information = new List<oM.Test.ITestInformation>();
+
+                        //Set up outputs for non-matches
+                        UT.UnitTest unmatchTest = new UT.UnitTest { Method = test.Method };  //Halfclone of the UT, keeping only the method
+                        TestResult unmatchResult = result.ShallowClone();    //Clone the result and wipe inner TestInformation
+                        matchResult.Information = new List<oM.Test.ITestInformation>();
+
                         for (int i = 0; i < test.Data.Count; i++)   //For each innner TestData point
                         {
                             if (result.Information[i].Status == status) //Status matching -> add Data to UT and test information to result
                             {
-                                filterTest.Data.Add(test.Data[i]);
-                                filterResult.Information.Add(result.Information[i]);
+                                matchTest.Data.Add(test.Data[i]);
+                                matchResult.Information.Add(result.Information[i]);
+                            }
+                            else    //Status not matching -> Add data and TestInformation no unmatching entries
+                            {
+                                unmatchTest.Data.Add(test.Data[i]);
+                                unmatchResult.Information.Add(result.Information[i]);
                             }
                         }
-                        if (filterTest.Data.Count != 0) //At least one inner match found
+
+                        if (matchTest.Data.Count != 0) //At least one inner match found
                         {
-                            filteredTests.Add(filterTest);
-                            filteredResults.Add(filterResult);
+                            matchingTests.Add(matchTest);
+                            matchingResults.Add(matchResult);
+                        }
+
+                        if (unmatchTest.Data.Count != 0) //At least one inner unmatch found
+                        {
+                            unmatchingTests.Add(unmatchTest);
+                            unmatchingResults.Add(unmatchResult);
                         }
                     }
                 }
@@ -95,16 +119,21 @@ namespace BH.Engine.UnitTest
             {
                 for (int i = 0; i < tests.Count; i++)   //Simly loop through the tests
                 {
-                    TestResult result = tests[i].CheckTest();   
+                    TestResult result = tests[i].CheckTest();
                     if (result.Status == status)    //And add if matching outer status
                     {
-                        filteredTests.Add(tests[i]);
-                        filteredResults.Add(result);
+                        matchingTests.Add(tests[i]);
+                        matchingResults.Add(result);
+                    }
+                    else    //If not, add to unmatch list
+                    {
+                        unmatchingTests.Add(tests[i]);
+                        unmatchingResults.Add(result);
                     }
                 }
             }
 
-            return new Output<List<UT.UnitTest>, List<TestResult>> { Item1 = filteredTests, Item2 = filteredResults };
+            return new Output<List<UT.UnitTest>, List<TestResult>, List<UT.UnitTest>, List<TestResult>> { Item1 = matchingTests, Item2 = matchingResults, Item3 = unmatchingTests, Item4 = unmatchingResults };
         }
 
         /***************************************************/
@@ -113,12 +142,14 @@ namespace BH.Engine.UnitTest
         [Input("testDataSet", "The test dataset to be evaluated.")]
         [Input("status", "Status looked for.")]
         [Input("filterData", "If true, returns new UnitTests objects where the data has been filtered to only contain TestData which returns the provided status. If false, returns full provided UnitTest if the overall status is matching the provided status.")]
-        [MultiOutput(0, "tests", "The filtered tests.")]
-        [MultiOutput(1, "results", "Results matching the filtered tests.")]
-        public static Output<List<UT.UnitTest>, List<TestResult>> FilterByCheckStatus(this Dataset testDataSet, BH.oM.Test.TestStatus status = oM.Test.TestStatus.Error, bool filterData = false)
+        [MultiOutput(0, "matchingTests", "The filtered tests with status matching the provided status.")]
+        [MultiOutput(1, "matchingResults", "Results matching the filtered tests.")]
+        [MultiOutput(2, "unmatchingTests", "Tests with a status other than the provided.")]
+        [MultiOutput(3, "unmatchingResults", "TestResults corresponding to the UnitTests not matching the status.")]
+        public static Output<List<UT.UnitTest>, List<TestResult>, List<UT.UnitTest>, List<TestResult>> FilterByCheckStatus(this Dataset testDataSet, BH.oM.Test.TestStatus status = oM.Test.TestStatus.Error, bool filterData = false)
         {
             if (testDataSet == null)
-                return new Output<List<UT.UnitTest>, List<TestResult>> { Item1 = new List<UT.UnitTest>(), Item2 = new List<TestResult>() };
+                return new Output<List<UT.UnitTest>, List<TestResult>, List<UT.UnitTest>, List<TestResult>> { Item1 = new List<UT.UnitTest>(), Item2 = new List<TestResult>(), Item3 = new List<UT.UnitTest>(), Item4 = new List<TestResult>() };
                 
             List<UT.UnitTest> unitTests = testDataSet.Data.OfType<UT.UnitTest>().ToList();
             return FilterByCheckStatus(unitTests, status, filterData);
@@ -130,14 +161,16 @@ namespace BH.Engine.UnitTest
         [Input("fileName", "The full file path to the file containing the serialised test datasets.")]
         [Input("status", "Status looked for.")]
         [Input("filterData", "If true, returns new UnitTests objects where the data has been filtered to only contain TestData which returns the provided status. If false, returns full provided UnitTest if the overall status is matching the provided status.")]
-        [MultiOutput(0, "tests", "The filtered tests.")]
-        [MultiOutput(1, "results", "Results matching the filtered tests.")]
-        public static Output<List<UT.UnitTest>, List<TestResult>> FilterByCheckStatus(this string fileName, BH.oM.Test.TestStatus status = oM.Test.TestStatus.Error, bool filterData = false)
+        [MultiOutput(0, "matchingTests", "The filtered tests with status matching the provided status.")]
+        [MultiOutput(1, "matchingResults", "Results matching the filtered tests.")]
+        [MultiOutput(2, "unmatchingTests", "Tests with a status other than the provided.")]
+        [MultiOutput(3, "unmatchingResults", "TestResults corresponding to the UnitTests not matching the status.")]
+        public static Output<List<UT.UnitTest>, List<TestResult>, List<UT.UnitTest>, List<TestResult>> FilterByCheckStatus(this string fileName, BH.oM.Test.TestStatus status = oM.Test.TestStatus.Error, bool filterData = false)
         {
             Dataset testSet = DatasetFromFile(fileName);
             if (testSet == null)
-                return new Output<List<UT.UnitTest>, List<TestResult>> { Item1 = new List<UT.UnitTest>(), Item2 = new List<TestResult>() };
-
+                return new Output<List<UT.UnitTest>, List<TestResult>, List<UT.UnitTest>, List<TestResult>> { Item1 = new List<UT.UnitTest>(), Item2 = new List<TestResult>(), Item3 = new List<UT.UnitTest>(), Item4 = new List<TestResult>() };
+            
             return FilterByCheckStatus(testSet, status, filterData);
         }
 
