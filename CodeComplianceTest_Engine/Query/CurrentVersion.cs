bf9dbd5bf25fa71213cf5b8d76e348d920615cb5
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2022-12-23 12:10:48 +0000

    Uptick to 6.1 milestone

--- CodeComplianceTest_Engine/Query/CurrentVersion.cs
+++ CodeComplianceTest_Engine/Query/CurrentVersion.cs
@@ -42,7 +42,7 @@ namespace BH.Engine.Test.CodeCompliance
 
         public static string CurrentAssemblyFileVersion()
         {
-            return "6.0"; //Update each milestone - don't forget the one above!
+            return "6.1"; //Update each milestone - don't forget the one above!
         }
 
         public static string FullCurrentAssemblyFileVersion()
