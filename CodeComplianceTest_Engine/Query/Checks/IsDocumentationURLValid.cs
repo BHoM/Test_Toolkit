2d80cedc54fb135f60a599161c53244ad4c3a3f0
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-02-07 17:07:27 +0000

    Add compliance check for documentation URL validity

--- CodeComplianceTest_Engine/Query/Checks/IsDocumentationURLValid.cs
+++ CodeComplianceTest_Engine/Query/Checks/IsDocumentationURLValid.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
  * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
  *
@@ -32,34 +32,41 @@ using System.Threading.Tasks;
 
 using BH.oM.Test;
 
+using System.Net;
+using Microsoft.CodeAnalysis;
+
 namespace BH.Engine.Test.CodeCompliance.Checks
 {
     public static partial class Query
     {
-        [Message("Method cannot contain more than one Output attribute", "HasUniqueOutputAttribute")]
+        [Message("Documentation link does not link to a valid web resource", "IsDocumentationURLValid")]
         [ErrorLevel(TestStatus.Error)]
-        [Path(@"([a-zA-Z0-9]+)_(Engine|Adapter)\\.*\.cs$")]
+        [Path(@"([a-zA-Z0-9]+)_(oM|Engine|Adapter)\\.*\.cs$")]
         [Path(@"([a-zA-Z0-9]+)_Engine\\Objects\\.*\.cs$", false)]
         [ComplianceType("documentation")]
-        public static Span HasUniqueOutputAttribute(this AttributeSyntax node)
+        public static Span IsDocumentationURLValid(this AttributeSyntax node)
         {
             if (node == null)
                 return null;
 
-            string name = node.Name.ToString();
-            if (name != "Output" && name != "MultiOutput")
+            if (node.Name.ToString() != "DocumentationURL")
                 return null;
 
-            var method = node.Parent.Parent as BaseMethodDeclarationSyntax;
-            if (method != null && method.IsPublic() && (method.IsEngineMethod() || method.IsAdapterConstructor()))
-            {
-                var outattrs = method.GetAttributes("Output");
-                if (outattrs.Count > 0 && outattrs[0] != node)
-                    return node.Span.ToSpan();
 
-                List<AttributeSyntax> multiOutAttrs = method.GetAttributes("MultiOutput");
-                if (multiOutAttrs.Where(x => x == node).Count() > 1)
+            var url = node.ArgumentList.Arguments[0].ToString().Replace("\"", "");
+
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "HEAD";
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                response.Close();
+                if (response.StatusCode != HttpStatusCode.OK)
                     return node.Span.ToSpan();
+            }
+            catch
+            {
+                return node.Span.ToSpan();
             }
 
             return null;
