2c9821f81f46a9aa7b9bc0ffd6d1bb63f1487596
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-02-14 12:49:59 +0000

    Update framework

--- NUnit_oM/Assertions.cs
+++ NUnit_oM/Assertions.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
-
-namespace BH.oM.Test.NUnit
-{
-    [XmlRoot(ElementName = "assertions")]
-    public class Assertions
-    {
-
-        [XmlElement(ElementName = "assertion")]
-        public Assertion Assertion { get; set; }
-    }
-}
+﻿
