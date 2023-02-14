4949469b9afacbaf913b99fb8a4d7c9732ced04a
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-02-14 12:51:36 +0000

    Sigh

--- NUnit_oM/Failure.cs
+++ NUnit_oM/Failure.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BH.oM.Test.NUnit
+{
+    [XmlRoot(ElementName = "failure")]
+    public class Failure
+    {
+
+        [XmlElement(ElementName = "message")]
+        public string Message { get; set; }
+
+        [XmlElement(ElementName = "stack-trace")]
+        public string Stacktrace { get; set; }
+    }
+}
--- NUnit_oM/Properties.cs
+++ NUnit_oM/Properties.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BH.oM.Test.NUnit
+{
+    [XmlRoot(ElementName = "properties")]
+    public class Properties
+    {
+
+        [XmlElement(ElementName = "property")]
+        public List<Property> Property { get; set; }
+    }
+}
