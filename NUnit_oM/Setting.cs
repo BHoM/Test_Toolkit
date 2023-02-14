b6fd374b41cc4b3106ed63a90442e0fe0f883268
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-02-14 12:51:36 +0000

    Sigh

--- NUnit_oM/Setting.cs
+++ NUnit_oM/Setting.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BH.oM.Test.NUnit
+{
+    [XmlRoot(ElementName = "setting")]
+    public class Setting
+    {
+
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+
+        [XmlAttribute(AttributeName = "value")]
+        public int Value { get; set; }
+    }
+}
