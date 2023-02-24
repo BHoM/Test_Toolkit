68aee75896de55d8043663d426b15d63abe634b6
Alessio Lombardi <alessio.lombardi@burohappold.com>
2023-02-24 16:58:50 +0000

    Added descriptions to TestRunner_Tests

--- .ci/unit-tests/NUnit_TestRunner_Tests/TestsRunner_Tests.cs
+++ .ci/unit-tests/NUnit_TestRunner_Tests/TestsRunner_Tests.cs
@@ -10,10 +10,15 @@ namespace NUnit_TestRunner_Tests
     public class TestsRunner_Tests
     {
         [Test]
+        [Description("Uses the test runner in BH.Engine.Test.NUnit.Compute.RunTests " +
+            "to run the tests written in the NUnit_Engine_Tests project and verifies that there are no errors.")]
         public void Run_NUnit_Engine_Tests()
         {
             string? solutionPath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)?.Parent?.Parent?.Parent?.Parent?.FullName;
-            string NUnit_Engine_Tests_path = Path.Combine(solutionPath ?? "", "NUnit_Engine_Tests\\bin\\Debug\\net6.0\\NUnit_Engine_Tests.dll");
+            if (solutionPath == null)
+                Assert.Fail("Could not find solution directory path.");
+
+            string NUnit_Engine_Tests_path = Path.Combine(solutionPath, "NUnit_Engine_Tests\\bin\\Debug\\net6.0\\NUnit_Engine_Tests.dll");
             
             TestRun testRunResult = Compute.RunTests(NUnit_Engine_Tests_path);
             Console.WriteLine(JsonConvert.SerializeObject(testRunResult, Formatting.Indented));
