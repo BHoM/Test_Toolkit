f4d7159d7a54e3d613c9f6214b23b6956b0a07db
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-03-14 10:59:30 +0000

    Update CSProject compliance check to include .Net 4.8

--- CodeComplianceTest_Engine/Compute/CheckProjectFile.cs
+++ CodeComplianceTest_Engine/Compute/CheckProjectFile.cs
@@ -99,7 +99,7 @@ namespace BH.Engine.Test.CodeCompliance
 
         private static TestResult CheckNETTarget(this ProjectFile csProject, List<string> fileLines, TestResult finalResult, string csProjFilePath, string documentationLink)
         {
-            List<string> acceptableNETTargets = new List<string> { "v4.7.2", "net472", "netstandard2.0", "net5.0", "net6.0" };
+            List<string> acceptableNETTargets = new List<string> { "v4.7.2", "net472", "net480", "net481", "netstandard2.0", "net5.0", "net6.0" };
             bool atLeastOneCorrect = false;
 
             foreach(string target in csProject.TargetNETVersions)
@@ -108,7 +108,7 @@ namespace BH.Engine.Test.CodeCompliance
                 {
                     string fullXMLText = $"<TargetFramework>{target}</TargetFramework>";
                     int lineNumber = fileLines.IndexOf(fileLines.Where(x => x.Contains(fullXMLText)).FirstOrDefault()) + 1; //+1 because index is 0 based but line numbers start at 1 for the spans
-                    finalResult = finalResult.Merge(Create.TestResult(TestStatus.Warning, new List<Error> { Create.Error($"Target frameworks for BHoM projects should either be .Net Framework 4.7.2, .Net Standard 2.0, or .Net 5.0.", Create.Location(csProjFilePath, Create.LineSpan(lineNumber, lineNumber)), documentationLink, TestStatus.Warning) }));
+                    finalResult = finalResult.Merge(Create.TestResult(TestStatus.Warning, new List<Error> { Create.Error($"Target frameworks for BHoM projects should either be .Net Framework 4.7.2, .Net Framework 4.8, .Net Framework 4.8.1, .Net Standard 2.0, .Net 5.0, or .Net 6.0.", Create.Location(csProjFilePath, Create.LineSpan(lineNumber, lineNumber)), documentationLink, TestStatus.Warning) }));
                 }
                 else
                     atLeastOneCorrect = true;
