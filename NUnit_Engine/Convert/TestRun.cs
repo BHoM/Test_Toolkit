d7ed9fb5dff7582b5b3023ecb005ecf85a28e5bc
Pawel Baran <pawel.baran@burohappold.com>
2023-05-08 14:11:55 +0200

    test running methods expanded

--- NUnit_Engine/Convert/TestRun.cs
+++ NUnit_Engine/Convert/TestRun.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
  * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
  *
@@ -20,46 +20,29 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Text;
-
-using BH.oM.Test.NUnit;
-using NUnit.Engine;
-using System.Xml.Serialization;
 using BH.oM.Base.Attributes;
+using BH.oM.Test.NUnit;
+using System.ComponentModel;
 using System.Xml;
-using System.Collections;
-using System.Reflection;
-using System.Linq;
+using System.Xml.Serialization;
 
 namespace BH.Engine.Test.NUnit
 {
-    public static partial class Compute
+    public static partial class Convert
     {
-        [Description("Runs a set of NUnit tests from a given DLL which contains code set up with the NUnit framework.")]
-        [Input("filePath", "A full file path to the DLL file which contains the NUnit tests.")]
-        [Output("testRun", "The NUnit test result from running the DLL.")]
-        public static TestRun RunTests(string filePath)
+        [Description("Convert an XML node returned by NUnit test run to a BHoM TestRun object that reflects the structure of the XML.")]
+        [Input("nunitRun", "The XML node returned by NUnit test run to convert.")]
+        [Output("testRun", "The converted BHoM TestRun.")]
+        public static TestRun ToTestRun(this XmlNode nunitRun)
         {
-            if (string.IsNullOrEmpty(filePath) || !File.Exists(filePath))
-                return null;
-
-            ITestEngine testEngine = TestEngineActivator.CreateInstance();
-            var package = new TestPackage(filePath);
-            var testRunner = testEngine.GetRunner(package);
-            var testResult = testRunner.Run(null, TestFilter.Empty);
-
             XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.LoadXml(testResult.OuterXml);
+            xmlDoc.LoadXml(nunitRun.OuterXml);
 
-            XmlSerializer ser = new XmlSerializer(typeof(TestRun));
+            XmlSerializer serialiser = new XmlSerializer(typeof(TestRun));
             TestRun result = null;
             using (XmlNodeReader reader = new XmlNodeReader(xmlDoc))
             {
-                result = ser.Deserialize(reader) as TestRun;
+                result = serialiser.Deserialize(reader) as TestRun;
             }
             return result;
         }
