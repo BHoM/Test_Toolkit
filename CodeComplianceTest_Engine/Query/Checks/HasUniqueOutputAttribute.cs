3981005b2101aab9070aaf44cacd4c11a799984c
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-05-17 10:42:34 +0100

    Exclude HasUniqueOutputAttribute from CI Unit Tests

--- CodeComplianceTest_Engine/Query/Checks/HasUniqueOutputAttribute.cs
+++ CodeComplianceTest_Engine/Query/Checks/HasUniqueOutputAttribute.cs
@@ -40,6 +40,7 @@ namespace BH.Engine.Test.CodeCompliance.Checks
         [ErrorLevel(TestStatus.Error)]
         [Path(@"([a-zA-Z0-9]+)_(Engine|Adapter)\\.*\.cs$")]
         [Path(@"([a-zA-Z0-9]+)_Engine\\Objects\\.*\.cs$", false)]
+        [Path(@"([a-zA-Z0-9]+)_Tests\\.*\.cs$", false)]
         [ComplianceType("documentation")]
         public static Span HasUniqueOutputAttribute(this AttributeSyntax node)
         {
