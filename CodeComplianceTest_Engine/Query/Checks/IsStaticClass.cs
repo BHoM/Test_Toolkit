a3fb01fb187c925bfbe792a0267229f626ddf932
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-05-17 10:46:44 +0100

    Exclude IsStaticClass for CI Unit Tests

--- CodeComplianceTest_Engine/Query/Checks/IsStaticClass.cs
+++ CodeComplianceTest_Engine/Query/Checks/IsStaticClass.cs
@@ -38,6 +38,7 @@ namespace BH.Engine.Test.CodeCompliance.Checks
         [Message("Invalid Engine class: Engine classes must be static", "IsStaticClass")]
         [Path(@"([a-zA-Z0-9]+)_Engine\\.*\.cs$")]
         [Path(@"([a-zA-Z0-9]+)_Engine\\Objects\\.*\.cs$", false)]
+        [Path(@"([a-zA-Z0-9]+)_Tests\\.*\.cs$", false)]
         [ComplianceType("code")]
         [ErrorLevel(TestStatus.Error)]
         public static Span IsStaticClass(this ClassDeclarationSyntax node)
