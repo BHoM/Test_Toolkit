effc8a50fbbb5be69f01bb8a07bfb5fff65f9c59
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-05-17 10:40:39 +0100

    Exclude HasOutputAttribute from CI Unit Tests

--- CodeComplianceTest_Engine/Query/Checks/HasOutputAttribute.cs
+++ CodeComplianceTest_Engine/Query/Checks/HasOutputAttribute.cs
@@ -40,6 +40,7 @@ namespace BH.Engine.Test.CodeCompliance.Checks
         [ErrorLevel(TestStatus.Warning)]
         [Path(@"([a-zA-Z0-9]+)_Engine\\.*\.cs$")]
         [Path(@"([a-zA-Z0-9]+)_Engine\\Objects\\.*\.cs$", false)]
+        [Path(@"([a-zA-Z0-9]+)_Tests\\.*\.cs$", false)]
         [IsPublic()]
         [ComplianceType("documentation")]
         public static Span HasOutputAttribute(this MethodDeclarationSyntax node)
