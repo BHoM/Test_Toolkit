b7fe02459e2dd4d0da874aea75f5a17903595777
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2023-09-25 10:57:35 +0100

    Uptick to 7.0

--- CodeComplianceTest_Engine/Query/CurrentVersion.cs
+++ CodeComplianceTest_Engine/Query/CurrentVersion.cs
@@ -32,7 +32,7 @@ namespace BH.Engine.Test.CodeCompliance
     {
         public static string CurrentAssemblyVersion()
         {
-            return "6.0"; //Update each year - don't forget the one below!
+            return "7.0"; //Update each year - don't forget the one below!
         }
 
         public static string FullCurrentAssemblyVersion()
@@ -42,7 +42,7 @@ namespace BH.Engine.Test.CodeCompliance
 
         public static string CurrentAssemblyFileVersion()
         {
-            return "6.3"; //Update each milestone - don't forget the one above!
+            return "7.0"; //Update each milestone - don't forget the one above!
         }
 
         public static string FullCurrentAssemblyFileVersion()
