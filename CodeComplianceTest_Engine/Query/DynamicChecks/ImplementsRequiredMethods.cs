25b8add05c187554f4a718dc40b148bf7b91e4ce
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2024-02-19 16:04:44 +0000

    New method for checking if an object type implements required IElement methods.

---
+++ CodeComplianceTest_Engine/Query/DynamicChecks/ImplementsRequiredMethods.cs
@@ -0,0 +1,360 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2024, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Test;
+using BH.oM.Test.Results;
+using BH.oM.Dimensional;
+using BH.oM.Test.CodeCompliance;
+using BH.oM.Base.Debugging;
+using BH.oM.Geometry;
+
+namespace BH.Engine.Test.CodeCompliance.DynamicChecks
+{
+    public static partial class Query
+    {
+        public static TestResult ImplementsRequiredMethods(Type type)
+        {
+            if (type == null)
+                return Create.TestResult(oM.Test.TestStatus.Pass); //Null type cannot need required methods and so cannot fail
+
+            BH.Engine.Base.Compute.LoadAllAssemblies(); //Just to be sure
+
+            var obj = BH.Engine.Test.Compute.DummyObject(type);
+            var objectTypeFullName = obj.GetType().FullName;
+
+            TestResult result = null;
+
+            if (typeof(IElement0D).IsAssignableFrom(type))
+                result = result.Merge(ImplementsIElement0DMethods(obj, objectTypeFullName));
+
+            if (typeof(IElement1D).IsAssignableFrom(type))
+                result = result.Merge(ImplementsIElement1DMethods(obj, objectTypeFullName));
+
+            if (typeof(IElement2D).IsAssignableFrom(type))
+                result = result.Merge(ImplementsIElement2DMethods(obj, objectTypeFullName));
+
+            if (typeof(IElementM).IsAssignableFrom(type))
+                result = result.Merge(ImplementsIElementMMethods(obj, objectTypeFullName));
+
+            return result;            
+        }
+
+        private static TestResult ImplementsIElement0DMethods(object obj, string objectFullName)
+        {
+            BH.Engine.Base.Compute.ClearCurrentEvents();
+
+            TestResult result = null;
+
+            var methodResult = BH.Engine.Base.Compute.RunExtensionMethod(obj, "Geometry");
+            if (methodResult == null || BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == EventType.Error && x.Message.Contains("Failed to run Geometry extension method")).Any())
+            {
+                Error error = new Error()
+                {
+                    Message = $"Geometry extension method not implemented for object of type {objectFullName}.",
+                    DocumentationLink = "", //ToDo
+                    Status = TestStatus.Error,
+                };
+
+                result = result.Merge(new TestResult()
+                {
+                    Description = $"Check if object of type {objectFullName} implements all required extension methods as an IElement0D object.",
+                    ID = "",
+                    Status = TestStatus.Error,
+                    Information = new List<ITestInformation>() { error },
+                });
+            }
+
+            BH.Engine.Base.Compute.ClearCurrentEvents();
+
+            Point pt = new Point();
+            methodResult = BH.Engine.Base.Compute.RunExtensionMethod(obj, "SetGeometry", new object[] { pt });
+            if (methodResult == null || BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == EventType.Error && x.Message.Contains("Failed to run SetGeometry extension method")).Any())
+            {
+                Error error = new Error()
+                {
+                    Message = $"SetGeometry extension method not implemented for object of type {objectFullName}.",
+                    DocumentationLink = "", //ToDo
+                    Status = TestStatus.Error,
+                };
+
+                result = result.Merge(new TestResult()
+                {
+                    Description = $"Check if object of type {objectFullName} implements all required extension methods as an IElement0D object.",
+                    ID = "",
+                    Status = TestStatus.Error,
+                    Information = new List<ITestInformation>() { error },
+                });
+            }
+
+            BH.Engine.Base.Compute.ClearCurrentEvents();
+
+            methodResult = BH.Engine.Base.Compute.RunExtensionMethod(obj, "HasMergeablePropertiesWith", new object[] { obj });
+            if (methodResult == null || BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == EventType.Error && x.Message.Contains("Failed to run HasMergeablePropertiesWith extension method")).Any())
+            {
+                Error error = new Error()
+                {
+                    Message = $"HasMergeablePropertiesWith extension method not implemented for object of type {objectFullName}.",
+                    DocumentationLink = "", //ToDo
+                    Status = TestStatus.Error,
+                };
+
+                result = result.Merge(new TestResult()
+                {
+                    Description = $"Check if object of type {objectFullName} implements all required extension methods as an IElement0D object.",
+                    ID = "",
+                    Status = TestStatus.Error,
+                    Information = new List<ITestInformation>() { error },
+                });
+            }
+
+            return result;
+        }
+
+        private static TestResult ImplementsIElement1DMethods(object obj, string objectFullName)
+        {
+            BH.Engine.Base.Compute.ClearCurrentEvents();
+
+            TestResult result = null;
+
+            var methodResult = BH.Engine.Base.Compute.RunExtensionMethod(obj, "Geometry");
+            if (methodResult == null || BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == EventType.Error && x.Message.Contains("Failed to run Geometry extension method")).Any())
+            {
+                Error error = new Error()
+                {
+                    Message = $"Geometry extension method not implemented for object of type {objectFullName}.",
+                    DocumentationLink = "", //ToDo
+                    Status = TestStatus.Error,
+                };
+
+                result = result.Merge(new TestResult()
+                {
+                    Description = $"Check if object of type {objectFullName} implements all required extension methods as an IElement1D object.",
+                    ID = "",
+                    Status = TestStatus.Error,
+                    Information = new List<ITestInformation>() { error },
+                });
+            }
+
+            BH.Engine.Base.Compute.ClearCurrentEvents();
+
+            Polyline pline = new Polyline() { ControlPoints = new List<Point>() { new Point(), new Point() } }; //0 length polyline for testing purposes only
+            methodResult = BH.Engine.Base.Compute.RunExtensionMethod(obj, "SetGeometry", new object[] { pline });
+            if (methodResult == null || BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == EventType.Error && x.Message.Contains("Failed to run SetGeometry extension method")).Any())
+            {
+                Error error = new Error()
+                {
+                    Message = $"SetGeometry extension method not implemented for object of type {objectFullName}.",
+                    DocumentationLink = "", //ToDo
+                    Status = TestStatus.Error,
+                };
+
+                result = result.Merge(new TestResult()
+                {
+                    Description = $"Check if object of type {objectFullName} implements all required extension methods as an IElement1D object.",
+                    ID = "",
+                    Status = TestStatus.Error,
+                    Information = new List<ITestInformation>() { error },
+                });
+            }
+
+            BH.Engine.Base.Compute.ClearCurrentEvents();
+
+            methodResult = BH.Engine.Base.Compute.RunExtensionMethod(obj, "HasMergeablePropertiesWith", new object[] { obj });
+            if (methodResult == null || BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == EventType.Error && x.Message.Contains("Failed to run HasMergeablePropertiesWith extension method")).Any())
+            {
+                Error error = new Error()
+                {
+                    Message = $"HasMergeablePropertiesWith extension method not implemented for object of type {objectFullName}.",
+                    DocumentationLink = "", //ToDo
+                    Status = TestStatus.Error,
+                };
+
+                result = result.Merge(new TestResult()
+                {
+                    Description = $"Check if object of type {objectFullName} implements all required extension methods as an IElement1D object.",
+                    ID = "",
+                    Status = TestStatus.Error,
+                    Information = new List<ITestInformation>() { error },
+                });
+            }
+
+            return result;
+        }
+
+        private static TestResult ImplementsIElement2DMethods(object obj, string objectFullName)
+        {
+            BH.Engine.Base.Compute.ClearCurrentEvents();
+
+            TestResult result = null;
+
+            var methodResult = BH.Engine.Base.Compute.RunExtensionMethod(obj, "OutlineElements1D");
+            if (methodResult == null || BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == EventType.Error && x.Message.Contains("Failed to run OutlineElements1D extension method")).Any())
+            {
+                Error error = new Error()
+                {
+                    Message = $"OutlineElements1D extension method not implemented for object of type {objectFullName}.",
+                    DocumentationLink = "", //ToDo
+                    Status = TestStatus.Error,
+                };
+
+                result = result.Merge(new TestResult()
+                {
+                    Description = $"Check if object of type {objectFullName} implements all required extension methods as an IElement2D object.",
+                    ID = "",
+                    Status = TestStatus.Error,
+                    Information = new List<ITestInformation>() { error },
+                });
+            }
+
+            BH.Engine.Base.Compute.ClearCurrentEvents();
+
+            Polyline pline = new Polyline() { ControlPoints = new List<Point>() { new Point(), new Point() } }; //0 length polyline for testing purposes only
+            List<IElement1D> outlines = new List<IElement1D>() { pline };
+
+            methodResult = BH.Engine.Base.Compute.RunExtensionMethod(obj, "SetOutlineElements1D", new object[] { outlines });
+            if (methodResult == null || BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == EventType.Error && x.Message.Contains("Failed to run SetOutlineElements1D extension method")).Any())
+            {
+                Error error = new Error()
+                {
+                    Message = $"SetOutlineElements1D extension method not implemented for object of type {objectFullName}.",
+                    DocumentationLink = "", //ToDo
+                    Status = TestStatus.Error,
+                };
+
+                result = result.Merge(new TestResult()
+                {
+                    Description = $"Check if object of type {objectFullName} implements all required extension methods as an IElement2D object.",
+                    ID = "",
+                    Status = TestStatus.Error,
+                    Information = new List<ITestInformation>() { error },
+                });
+            }
+
+            BH.Engine.Base.Compute.ClearCurrentEvents();
+
+            methodResult = BH.Engine.Base.Compute.RunExtensionMethod(obj, "HasMergeablePropertiesWith", new object[] { obj });
+            if (methodResult == null || BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == EventType.Error && x.Message.Contains("Failed to run HasMergeablePropertiesWith extension method")).Any())
+            {
+                Error error = new Error()
+                {
+                    Message = $"HasMergeablePropertiesWith extension method not implemented for object of type {objectFullName}.",
+                    DocumentationLink = "", //ToDo
+                    Status = TestStatus.Error,
+                };
+
+                result = result.Merge(new TestResult()
+                {
+                    Description = $"Check if object of type {objectFullName} implements all required extension methods as an IElement2D object.",
+                    ID = "",
+                    Status = TestStatus.Error,
+                    Information = new List<ITestInformation>() { error },
+                });
+            }
+
+            return result;
+        }
+
+        private static TestResult ImplementsIElementMMethods(object obj, string objectFullName)
+        {
+            BH.Engine.Base.Compute.ClearCurrentEvents();
+
+            TestResult result = null;
+
+            var methodResult = BH.Engine.Base.Compute.RunExtensionMethod(obj, "MaterialComposition");
+            if (methodResult == null || BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == EventType.Error && x.Message.Contains("Failed to run MaterialComposition extension method")).Any())
+            {
+                Error error = new Error()
+                {
+                    Message = $"MaterialComposition extension method not implemented for object of type {objectFullName}.",
+                    DocumentationLink = "", //ToDo
+                    Status = TestStatus.Error,
+                };
+
+                result = result.Merge(new TestResult()
+                {
+                    Description = $"Check if object of type {objectFullName} implements all required extension methods as an IElementM object.",
+                    ID = "",
+                    Status = TestStatus.Error,
+                    Information = new List<ITestInformation>() { error },
+                });
+            }
+
+            BH.Engine.Base.Compute.ClearCurrentEvents();
+
+            methodResult = BH.Engine.Base.Compute.RunExtensionMethod(obj, "SolidVolume");
+            if (methodResult == null || BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == EventType.Error && x.Message.Contains("Failed to run SolidVolume extension method")).Any())
+            {
+                Error error = new Error()
+                {
+                    Message = $"SolidVolume extension method not implemented for object of type {objectFullName}.",
+                    DocumentationLink = "", //ToDo
+                    Status = TestStatus.Error,
+                };
+
+                result = result.Merge(new TestResult()
+                {
+                    Description = $"Check if object of type {objectFullName} implements all required extension methods as an IElementM object.",
+                    ID = "",
+                    Status = TestStatus.Error,
+                    Information = new List<ITestInformation>() { error },
+                });
+            }
+
+            BH.Engine.Base.Compute.ClearCurrentEvents();
+
+            if(result != null)
+            {
+                //For IElementM, it is either the above two methods OR VolumetricMaterialTakeoff - so if the result is not null, it means we haven't found one or both of the above methods.
+                //If we have VolumetricMaterialTakeoff then we can reset the result
+
+                methodResult = BH.Engine.Base.Compute.RunExtensionMethod(obj, "VolumetricMaterialTakeoff");
+                if (methodResult == null || BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == EventType.Error && x.Message.Contains("Failed to run VolumetricMaterialTakeoff extension method")).Any())
+                {
+                    Error error = new Error()
+                    {
+                        Message = $"VolumetricMaterialTakeoff extension method not implemented for object of type {objectFullName}.",
+                        DocumentationLink = "", //ToDo
+                        Status = TestStatus.Error,
+                    };
+
+                    result = result.Merge(new TestResult()
+                    {
+                        Description = $"Check if object of type {objectFullName} implements all required extension methods as an IElementM object.",
+                        ID = "",
+                        Status = TestStatus.Error,
+                        Information = new List<ITestInformation>() { error },
+                    });
+                }
+                else
+                    result = null; //We do have a VolumetricMaterialTakeoff method, so the other 2 not existing is not a problem
+            }
+
+            return result;
+        }
+    }
+}
