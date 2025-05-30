d81dd21947cb3d0c112b81611498d2b7fe9788d6
Isak Näslund <isak.naslund@burohappold.com>
2025-05-30 10:10:28 +0100

    Base abstract runner for compliance

---
+++ TestSetup_Engine/Compliance/ComplianceTestBase.cs
@@ -0,0 +1,89 @@
+﻿using BH.Engine.Base;
+using BH.Engine.Test;
+using BH.oM.Base.Attributes;
+using BH.oM.Test;
+using BH.oM.Test.NUnit;
+using BH.oM.Test.Results;
+using BH.oM.Test.UnitTests;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BH.Engine.Test.CodeCompliance;
+using BH.oM.Test.CodeCompliance;
+
+namespace BH.Tests.Setup
+{
+    public abstract class ComplianceTestBase : NUnitTest
+    {
+
+        [TestCaseSource("TestFiles")]
+        public void RunCodeCompliance(string filePath)
+        {
+            RunCompliance(filePath, "code");
+        }
+
+        [TestCaseSource("TestFiles")]
+        public void RunDocumentationCompliance(string filePath)
+        {
+            RunCompliance(filePath, "documentation");
+        }
+
+        private void RunCompliance(string filePath, string checkType)
+        {
+
+            Assert.That(filePath, Is.Not.Null);
+            Assert.That(checkType, Is.Not.Null);
+
+            Assert.That(File.Exists(filePath), Is.True, "File does not exist.");
+
+            TestResult result = BH.Engine.Test.CodeCompliance.Compute.RunChecks(filePath, checkType);
+
+            Assert.Multiple(() =>
+            {
+                //Assert.That(result.Status == oM.Test.TestStatus.Pass, $"The ut did not pass {result.FullMessage(3, oM.Test.TestStatus.Error)}");
+                if (result.Status != TestStatus.Pass)
+                {
+                    List<Error> errors = GroupInformation(result.Information, filePath);
+                    foreach (Error error in errors)
+                    {
+                        string message = $"{error.Location.FilePath} line {error.Location.Line.Start.Line}:{error.Location.Line.End.Line}\n";
+                        message += error.Message;
+                        if (error.Status == TestStatus.Error)
+                            Assert.Fail(message);
+                        else
+                            Assert.Warn(message);
+                    }
+                }
+
+            });
+        }
+
+        [OneTimeSetUp]
+        public void EnsureStaticMembers()
+        {
+            var testDataMethod = this.GetType().GetMethod("TestFiles", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+            Assume.That(testDataMethod != null, "Expected static member TestFiles is not implemented on derrived test class");
+
+            Assume.That(testDataMethod.ReturnType == typeof(IEnumerable<string>), $"Expected return type of TestFiles() does not match expected IEnumerable<string>");
+        }
+
+        public static IEnumerable<string> GetCsFiles(string folder)
+        {
+            return Directory.GetFiles(Path.Combine(Query.CurrentRepoFolder(), folder), "*.cs", SearchOption.AllDirectories);
+        }
+
+        public static List<Error> GroupInformation(List<ITestInformation> information, string filePath)
+        {
+            List<Error> errors = information.OfType<Error>().ToList();
+
+            errors = errors.GroupErrors(filePath);
+
+            return errors;
+        }
+    }
+}
