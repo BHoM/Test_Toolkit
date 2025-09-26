c32fc2e00e64d87fa3e831ce1ab2bc4f62447730
Isak Näslund <isak.naslund@burohappold.com>
2025-09-26 14:19:41 +0200

    Further simplification and descriptions of the compliance runner

--- Compliance_Tests/CsFile.cs
+++ Compliance_Tests/CsFile.cs
@@ -16,71 +16,65 @@ using System.Text;
 
 namespace BH.Tests.Compliance
 {
+    [Description("Class that executes compliance checks for .cs files. The methods provided to the test fixture are the various check methods available in CodeComplianceTest_Engine." +
+                 "This will isntanciate a test class for each test method, and then the files are all checked against all method")]
     [TestFixtureSource(nameof(TestMethods))]
     public class CsFile
     {
-        private MethodInfo m_Method;
+        /***************************************************/
+        /**** Private fields                            ****/
+        /***************************************************/
 
-        public CsFile(string methodName)
+        private MethodInfo m_Method;    //Check method
+
+        /***************************************************/
+        /**** Constructor                               ****/
+        /***************************************************/
+
+        public CsFile(MethodInfo method)
         {
-            m_Method = m_checkMethods[methodName];
+            m_Method = method;
         }
 
-        private static Dictionary<string, SyntaxNode> m_Nodes = new Dictionary<string, SyntaxNode>();
-        private static object m_nodeLock = new object();
+        /***************************************************/
+        /**** Test methods                              ****/
+        /***************************************************/
 
-        private static SyntaxNode GetNode(string fileName)
-        {
-            if( m_Nodes.TryGetValue(fileName, out SyntaxNode node))
-                return node;
-
-            lock (m_nodeLock)
-            {
-                if (m_Nodes.TryGetValue(fileName, out node))
-                    return node;
-                fileName = System.IO.Path.GetFullPath(fileName);
-                string file;
-                using (StreamReader sr = new StreamReader(fileName))
-                {
-                    file = sr.ReadToEnd();
-                }
-                node = BH.Engine.Test.CodeCompliance.Convert.ToSyntaxTree(file, fileName).GetRoot();
-                m_Nodes[fileName] = node;
-                return node;
-            }
-        }
-
-        [TestCaseSource(typeof(BH.Tests.Setup.Query), nameof(BH.Tests.Setup.Query.TestFiles), new object[] {"cs"})]
+        [Description("Test method being executed for the current test method, and current .cs file.")]
+        [TestCaseSource(typeof(BH.Tests.Setup.Query), nameof(BH.Tests.Setup.Query.TestFilesCs))]
         public void TestCompliance(string fileName)
         {
-            SyntaxNode node = GetNode(fileName);
+            SyntaxNode node = GetNode(fileName);    
             MethodInfo method = m_Method;
             Assume.That(method != null);
             Assume.That(node != null);
             Assume.That(System.IO.Path.GetFileName(node.SyntaxTree.FilePath), Is.Not.EqualTo("AssemblyInfo.cs"), "Skipping AssemblyInfo.cs files.");
             
-            Assume.That(method.GetCustomAttributes<PathAttribute>().Select(condition => condition.IPasses(node)), Is.All.True, $"Method {method.Name} is not applicable to be tested with the {node.SyntaxTree.FilePath}.");
-
-            Assert.Multiple(() =>
+            if (method.GetCustomAttributes<PathAttribute>().All(condition => condition.IPasses(node)))  //Prefilter out files that don't match the path condition. Not really required (handled internally as well) but speeds up the execution
             {
-                CheckMethod(method, node, node.SyntaxTree.FilePath);
-            });
+                Assert.Multiple(() =>   //Allow multiple assertions to be raised for the same file
+                {
+                    CheckMethod(method, node, node.SyntaxTree.FilePath);
+                });
+            }
         }
 
+        /***************************************************/
+
+        [Description("Main validator method handling checking and assertion raising for the provided method and node.")]
         private static void CheckMethod(MethodInfo method, SyntaxNode node, string filePath)
         {
             Type type = node.GetType();
 
-
-            if (method.GetParameters()[0].ParameterType.IsAssignableFrom(type) &&
-                    !(typeof(MemberDeclarationSyntax).IsAssignableFrom(node.GetType())
+            if (method.GetParameters()[0].ParameterType.IsAssignableFrom(type) &&   //Check that the method can handle this type of node
+                    !(typeof(MemberDeclarationSyntax).IsAssignableFrom(type) // Ignore deprecated members
                     && ((MemberDeclarationSyntax)node).IsDeprecated()) &&
-                    method.GetCustomAttributes<ConditionAttribute>().All(condition => condition.IPasses(node)))
+                    method.GetCustomAttributes<ConditionAttribute>().All(condition => condition.IPasses(node))) //Check all conditions are met
             {
-                Func<object[], object> fn = GetFunction(method);
+                Func<object[], object> fn = GetFunction(method);    //Get compiled function of the compliance test method
 
-                Span result = fn(new object[] { node }) as Span;
-                if (result != null)
+                Span result = fn(new object[] { node }) as Span;    //Execute the method
+                if (result != null)     //If a result is returned, then raise the appropriate assertion. Null return means no issue found
                 {
                     string message = method.GetCustomAttribute<MessageAttribute>()?.Message ?? "";
                     string documentation = method.GetCustomAttribute<MessageAttribute>()?.DocumentationLink ?? "";
@@ -107,43 +101,65 @@ namespace BH.Tests.Compliance
 
             foreach (var child in node.ChildNodes())
             {
-                CheckMethod(method, child, filePath);
+                CheckMethod(method, child, filePath);   //Recurse through all child nodes
             }
 
         }
 
-       private static IEnumerable<TestFixtureData> TestMethods()
-        {
-            if (m_checkMethods == null)
-            {
-                lock (m_compileLock)
-                {
-                    if (m_checkMethods == null)
-                        m_checkMethods = new Dictionary<string, MethodInfo>();
+        /***************************************************/
+        /**** Test data methods                         ****/
+        /***************************************************/
 
-                    foreach (var methodGroup in BH.Engine.Test.CodeCompliance.Query.AllChecks().Distinct().GroupBy(x => x.Name))
+        [Description("Returns the test methods available in CodeComplianceTest_Engine to be executed as test fixtures.")]
+        private static IEnumerable<TestFixtureData> TestMethods()
+        {
+            foreach (var methodGroup in BH.Engine.Test.CodeCompliance.Query.AllChecks().Distinct().GroupBy(x => x.Name))
+            {
+                if (methodGroup.Count() == 1)
+                {
+                    yield return new TestFixtureData(methodGroup.First()).SetArgDisplayNames(methodGroup.First().Name);
+                }
+                else
+                {
+                    foreach (var method in methodGroup)
                     {
-                        if(methodGroup.Count() == 1)
-                        {
-                            m_checkMethods[methodGroup.First().Name] = methodGroup.First();
-                        }
-                        else
-                        {
-                            foreach (var method in methodGroup)
-                            {
-                                string key = method.Name + ": " + method.GetParameters().First().ParameterType.Name.Replace("DeclarationSyntax", "");
-                                m_checkMethods[key] = method;
-                            }
-                        }
+                        string key = method.Name + ": " + method.GetParameters().First().ParameterType.Name.Replace("DeclarationSyntax", "");
+                        yield return new TestFixtureData(method).SetArgDisplayNames(key);
                     }
                 }
+
             }
-
-            foreach (var methodName in m_checkMethods.Keys)
-                yield return new TestFixtureData(methodName).SetArgDisplayNames(methodName);
-
         }
 
+        /***************************************************/
+        /**** Extraction, compilation and cashing       ****/
+        /***************************************************/
+
+        [Description("Returns the syntax node for the provided file name, using a cache to avoid reloading and reparsing files.")]
+        private static SyntaxNode GetNode(string fileName)
+        {
+            if (m_Nodes.TryGetValue(fileName, out SyntaxNode node))
+                return node;
+
+            lock (m_nodeLock)
+            {
+                if (m_Nodes.TryGetValue(fileName, out node))
+                    return node;
+                fileName = System.IO.Path.GetFullPath(fileName);
+                string file;
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    file = sr.ReadToEnd();
+                }
+                node = BH.Engine.Test.CodeCompliance.Convert.ToSyntaxTree(file, fileName).GetRoot();
+                m_Nodes[fileName] = node;
+                return node;
+            }
+        }
+
+        /***************************************************/
+
+        [Description("Returns a compiled function for the provided method, using a cache to avoid recompiling methods.")]
         private static Func<object[], object> GetFunction(MethodInfo method)
         {
             Func<object[], object> fn;
@@ -158,9 +174,16 @@ namespace BH.Tests.Compliance
             return fn;
         }
 
-        private static Dictionary<string, MethodInfo> m_checkMethods = null;
+        /***************************************************/
+        /**** Caches and lock fields                    ****/
+        /***************************************************/
+
+        private static Dictionary<string, SyntaxNode> m_Nodes = new Dictionary<string, SyntaxNode>();
+        private static object m_nodeLock = new object();
 
         private static Dictionary<MethodInfo, Func<object[], object>> m_checkMethodFunctions = new Dictionary<MethodInfo, Func<object[], object>>();
         private static object m_compileLock = new object();
+
+        /***************************************************/
     }
 }
--- Compliance_Tests/ProjectCompliance.cs
+++ Compliance_Tests/ProjectCompliance.cs
@@ -14,8 +14,13 @@ namespace BH.Tests.Compliace
 {
     public class ProjectCompliance
     {
+        /***************************************************/
+        /**** Test methods                              ****/
+        /***************************************************/
 
-        [TestCaseSource(typeof(BH.Tests.Setup.Query), nameof(BH.Tests.Setup.Query.TestFiles), new object[] { "csproj" })]
+        [Description("Checks a .csproj file by using the method available in CodeComplianceTest_Engine." +
+                     "Potential to port the content of that method over to this file, split as individual tests, each run on the .csproj test files.")]
+        [TestCaseSource(typeof(BH.Tests.Setup.Query), nameof(BH.Tests.Setup.Query.TestFilesCsproj))]
         public void TestCompliance(string fileName)
         {
             TestResult result = BH.Engine.Test.CodeCompliance.Compute.CheckProjectFile(fileName);
@@ -28,5 +33,7 @@ namespace BH.Tests.Compliace
             if (result.Status == TestStatus.Warning)
                 Assert.Warn($"{fileName}: {result.FullMessage(5, TestStatus.Warning)}");
         }
+
+        /***************************************************/
     }
 }
